735e553f84f4d59d67ce9ec0d54b7421f23fa994
shifad878-prog <shifad878@gmail.com>
2026-02-23 10:52:57 +0500

    Done Chapter 2

---
+++ app.js
@@ -0,0 +1,107 @@
+// var studentName = "Shifa"
+// var studentAge = 19
+// var studentQuailification = "Intermediate"
+// var studentCourse = "BS Computer Science"
+
+// console.log(studentName);
+// console.log(studentAge);
+// console.log(studentQuailification);
+// console.log(studentCourse);
+
+// document.write(
+//     studentName
+// )
+// document.write("<br/>")
+
+// document.write(
+//     studentAge
+// )
+// document.write("<br/>")
+
+// document.write(
+//     studentQuailification
+// )
+
+// document.write("<br/>")
+
+// document.write(
+//     studentCourse
+// )
+
+
+// chapter No 1
+// Question No 1 
+
+// alert("Hey Welcome to our website!");
+
+// Question No 2 
+
+// document.write("Welcome to my website! Hope you have a great time.");
+
+// Question No 3 
+
+// alert("Welcome to JS! Land \n happy coding ")
+
+// Question 4 
+
+// alert("Welcome to JS Land");
+
+// alert("Happy Coding!");
+
+// Question 5
+// alert("Hello... I can run JS through my web browser's console");
+// console.log("Hello... I can run JS through my web browser's console"); 
+
+// Chapter 2 
+
+// Question # 1 
+
+let userName ;
+
+// Question # 2
+
+let myName = "Shifa Danish";
+document.write(myName)
+
+// Question # 3
+
+// a) 
+let message
+
+// b)  
+message = "Hellow World";
+
+//c) 
+alert(message)
+
+// Question # 4
+
+let studentName = "Shifa";
+let studentAge = 19;
+let studentCourse = "Moedren Web & App Development";
+
+// Question # 5
+
+let pizza = "PIZZA\n PIZZ \n PIZ\n PI\n P";
+alert(pizza);
+
+// Question # 6
+
+let email = "shifad878@gmail.com";
+
+alert("my email is " +  email);
+
+// Question # 7
+
+let book = "I am trying to learn from the Book A smarter way to learn JacaScript";
+
+alert(book);
+
+// Question # 8
+
+let javaScript = "Yah! I can write HTML content through JavaScript"
+document.write(javaScript);
+alert(javaScript);
+
+// Question # 9
+

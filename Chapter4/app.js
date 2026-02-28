0518c6aaa2ef812565e8ebbbcab49a019ddae59c
shifad878-prog <shifad878@gmail.com>
2026-02-28 23:22:20 +0500

    Done Chapter-4

---
+++ Chapter4/app.js
@@ -0,0 +1,41 @@
+// Question# 1
+
+let studentName =" Shifa"; studentAge = 19; studentCourse = "Modern Web & App Develoment";
+
+// Question# 2
+
+// legal variables
+
+let userName
+let userName1
+let $userName
+let user_Name
+let username
+
+// illegal Variables
+
+// let username
+// let User user
+// let 12userName
+// let @userName
+// let var-userName
+
+
+// Question# 3
+// a)
+document.write("<h1>Rules for naming JS variables</h1>");
+// b)
+document.write("Variable names can only contain letters, numbers, dollar signs ($) and underscores (_).");
+
+document.write("<br/>")
+// c)
+document.write("Variable names can only contain letters, numbers, dollar signs ($) and underscores (_).");
+
+document.write("<br/>")
+// d) 
+document.write("Variable names are case sensitive");
+
+document.write("<br/>")
+
+// e)
+document.write("Variable name should not be JS keywords");

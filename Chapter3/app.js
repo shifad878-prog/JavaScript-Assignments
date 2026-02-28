d2581881e32aea397f50db97bea05d09b5b964d1
shifad878-prog <shifad878@gmail.com>
2026-02-28 22:50:45 +0500

    Done Chapter-3

---
+++ Chapter3/app.js
@@ -0,0 +1,26 @@
+// Question 1
+// let age = 19;
+// alert("I am " + 19 + " years old");
+
+// Question 2
+// let visit = "You have visited this site N times";
+// alert("You have visited this site " + 14 +  " times")
+
+// Question 3
+
+let birthYear = 2006;
+let type = typeof birthYear
+document.write("My birth year is " +  2006)
+document.write("<br />");
+document.write("Data type of my decalared variable is " + type);
+
+// Question 4
+
+let visitorName = "John Doe";
+let productTitle = "T-shirt";
+let quantity = 5
+
+document.write( "<br />")
+document.write(visitorName + "ordered " + quantity + " " + productTitle + "s on XYZ Clothing store");
+
+

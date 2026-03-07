f5816825d96e6b6575247b8822934ae8d0bd5853
shifad878-prog <shifad878@gmail.com>
2026-03-07 19:26:53 +0500

    Done Chapter-5

---
+++ Chapter5/app.js
@@ -0,0 +1,151 @@
+// Arithimetic Operator
+// Question 1 
+
+let a = 3;
+let b = 5;
+
+let sum = a + b;
+document.write("sum of " + a + " & " + b + " is " + sum);
+
+document.write( "<br />");
+
+// Question 2 
+// a)
+let num1 = 3;
+let num2 = 5;
+
+let sub = num1 - num2;
+document.write("Subtraction of " + num1 + " & " + num2 + " is " + sub);
+
+document.write("<br/>");
+
+// b)
+
+let num3 = 3;
+let num4 = 5;
+
+let multi = num3 * num4;
+
+document.write("Multiplication of " + num3 + " & " + num4 + " is " + multi);
+
+document.write("<br/>");
+
+// c)
+
+let num5 = 3;
+let num6 = 5;
+
+let division = num5 / num6;
+
+document.write("Divisin of " + num5 + " & " + num6 + " is " + division)
+
+document.write("<br/>");
+
+// d)
+
+let num7 = 3;
+let num8 = 5;
+
+let mode = num7 % num8;
+
+document.write("Modulus of " + num7 + " & " + num8 + "is " + mode);
+document.write("<br/>")
+
+// Question 3
+//  a)
+let number;
+
+// b)
+document.write("value after  variable declaration is: undefined")
+
+// c)
+number = 5;
+document.write("<br/>")
+
+// d)
+document.write("Initial Value: 5.")
+document.write("<br/>")
+
+// e)
+
+document.write("number++");
+document.write("<br/>")
+
+// f)
+document.write("Value after increment is: 6");
+document.write("<br/>");
+
+// g)
+
+let num = 2
+
+num = num+7
+
+document.write(num)
+document.write("<br/>")
+
+// h)
+let no = 12;
+no++;
+document.write(no)
+document.write("<br/>")
+
+
+document.write("Value after addition is: 13");
+document.write("<br/>")
+
+// i)
+
+let minus = 12;
+minus--;
+document.write(minus)
+document.write("<br/>")
+
+// j)
+
+let decrement = 13;
+decrement--;
+document.write(decrement)
+document.write("<br/>")
+document.write("Value after decrement is: 12")
+document.write("<br/>")
+
+// k)
+let divid = 12 % 3;
+
+document.write(divid)
+document.write("<br/>")
+
+// l)
+
+document.write("The remainder is : 0");
+
+document.write("<br/>")
+
+
+// Question 4
+
+
+let valueOfTicket = 600;
+let totalPeople = 5;
+
+let Total = valueOfTicket * totalPeople
+document.write("Total cost to buy " + totalPeople + " tickets to a moviem " + Total + "PKR"); 
+document.write("<br/>");
+
+// Question 5
+
+let table = 4;
+
+document.write("table of " + table);
+document.write("<br/>");
+document.write(table + " x 1 = " + (table * 1) + "<br>");
+document.write(table + " x 2 = " + (table * 2) + "<br>");
+document.write(table + " x 3 = " + (table * 3) + "<br>");
+document.write(table + " x 4 = " + (table * 4) + "<br>");
+document.write(table + " x 5 = " + (table * 5) + "<br>");
+document.write(table + " x 6 = " + (table * 6) + "<br>");
+document.write(table + " x 7 = " + (table * 7) + "<br>");
+document.write(table + " x 8 = " + (table * 8) + "<br>");
+document.write(table + " x 9 = " + (table * 9) + "<br>");
+document.write(table + " x 10 = " + (table * 10) + "<br>");
